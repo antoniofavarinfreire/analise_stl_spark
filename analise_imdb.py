1ae4937f80d7a97dfa3fe083082df0e4cd58beec
Antonio Favarin Freire <42979335+antoniofavarinfreire@users.noreply.github.com>
2022-10-28 11:54:51 -0300

    Ajuste para apresentação

--- analise_imdb.py
+++ analise_imdb.py
@@ -11,14 +11,12 @@ spark = (
 )
 #criando dataframe
 df_title = spark.read.option("header", "true").option("sep", "\t").option("multiLine","true").option("quote","\"").option("scape","\"").option("ignoreTrailingWhiteSpace", "true").csv("title.basic.tsv")
-#df_title.show()
-
+df_title.show()
 
 #dataFrame titles
 df_rating = spark.read.option("header", "true").option("sep", "\t").option("multiLine","true").option("quote","\"").option("scape","\"").option("ignoreTrailingWhiteSpace", "true").csv("title.ratings.tsv")
-#df_rating.show()
+df_rating.show()
 
 df_title.join(df_rating, df_title.tconst == df_rating.tconst, 'inner')\
         .select(df_title.primaryTitle, df_title.originalTitle, df_title.startYear, df_title.genres, df_rating.averageRating)\
         .show(truncate=False)
-
